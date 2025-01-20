2583c808f8ab7588ecef47e5370cc70b0df8aff6
tomlukewarden <tomlukeprogramming@outlook.com>
2025-01-20 21:29:14 +0000

    added menu

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 
-import csv
 '''
+import csv
+
 from datetime import datetime
 '''
 # ADD EXPENSES BY TYPE
@@ -13,38 +14,72 @@ from datetime import datetime
 def expense_type():
     types = ['Bills', 'Groceries', 'Fun', 'Savings', 'Other']
     print(f"Expense Types: {', '.join(types)}")
-    category = input("What category is this expense? ")
-    
-    if category in types:
-        print(f"You selected: {category}")
-        return category
-    else:
-        print("Invalid category. Please try again.")
-        return expense_type()
+    while True:
+        category = input("What category is this expense? ")
+        if category in types:
+            print(f"You selected: {category}")
+            return category
+        else:
+            print("Invalid category. Please try again.")
 
 def income_type():
     types = ['Work', 'Side Job', 'Other']
     print(f"Income Types: {', '.join(types)}")
-    category = input("What category is this form of income? ")
-    
-    if category in types:
-        print(f"You selected: {category}")
-        return category
-    else:
-        print("Invalid category. Please try again.")
-        return income_type()
-    
+    while True:
+        category = input("What category is this form of income? ")
+        if category in types:
+            print(f"You selected: {category}")
+            return category
+        else:
+            print("Invalid category. Please try again.")
+
 def expense():
-    name = input('please enter the name of this expense')
-    amonut = input('Please Enter the amount of this expense: ')
-    category = expense_type()
-    print(f'Expense to Add: {name}, £{amonut}, {category}')
-    expense()
+    while True:
+        name = input('Please enter the name of this expense: ')
+        amount = input('Please enter the amount of this expense: ')
+        category = expense_type()
+        print(f'Expense to Add: {name}, £{amount}, {category}')
+        
+        another = input("Do you want to add another expense? (yes/no): ").lower()
+        if another != 'yes':
+            break
 
 def income():
-    name = input('please enter the name of this income')
-    amonut = input('Please Enter the amount of this income: ')
-    category = income_type()
-    print(f'Expense to Add: {name}, £{amonut}, {category}')
-    income()
+    while True:
+        name = input('Please enter the name of this income: ')
+        amount = input('Please enter the amount of this income: ')
+        category = income_type()
+        print(f'Income to Add: {name}, £{amount}, {category}')
+        
+        another = input("Do you want to add another income? (yes/no): ").lower()
+        if another != 'yes':
+            break
+def view_expenses():
+    print("Expenses: ")
+    # Read expenses from a file or database and print them here
+    
+def view_income():
+    print("Income: ")
+    # Read income from a file or database and print them here
 
+def menu():
+    while True:
+        print("1. Add Expense")
+        print("2. Add Income")
+        print("3. View Expenses")
+        print("4. View Income")
+        print("5. Exit")
+
+        choice = input("Please select an option: ")
+        if choice == '1':
+            expense()
+        elif choice == '2':
+            income()
+        elif choice == '3':
+            view_expenses()
+        elif choice == '4':
+            view_income()
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice. Please try again.")
